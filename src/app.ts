7364d7683d4f3a577ada6d49176fdde8de06dca2
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-04-30 22:31:16 -0500

    leer logs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import { Server } from "./presentation/server";
+import { envs } from "./config/plugins/envs.plugin";
+import { PrismaClient } from "@prisma/client";
 
 (async () => {
   main();
 })();
 
 function main() {
+  const prisma = new PrismaClient();
+  const newLog = prisma.logModel.create({
+    data: {
+      level: "HIGH",
+      message: "Test message",
+      origin: "App.ts",
+    },
+  });
+  console.log({ newLog });
+
+  const logs = prisma.logModel.findMany({
+    //busca solo por los logs en high
+    where: {
+      level: "HIGH",
+    },
+  });
+  console.log({ logs });
+
   Server.start();
   //console.log(envs.PORT);
 }
